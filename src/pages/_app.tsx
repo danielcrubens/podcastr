5b4a2429068d9f4e8afaf5707bb1e183d7d3b464
Daniel Costa Rubens <drubens@dcc.local>
2021-04-23 21:08:31 -0300

    refactoring :fire:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,19 +3,21 @@ import{Header} from '../components/Header';
 import { Player } from '../components/Player';
 
 
+
 import styles from '../styles/app.module.scss';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 function MyApp({ Component, pageProps }) {
-  return (
+return(
+  <PlayerContextProvider>
    <div className={styles.wrapper}>
     <main>
     <Header/>
     <Component{...pageProps}/>
     </main>
     <Player/>
-   </div>
-     
-   
-  );
+   </div>   
+   </PlayerContextProvider>
+  )
 }
 
 export default MyApp
